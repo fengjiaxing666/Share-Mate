4d0229f5ed5c7d910d3d6fae7371e847ef20005c
fengjiaxing666 <873086452@qq.com>
2018-12-02 11:44:22 +0800

    用户登录注册页面压缩包

--- login/ShareMate/library/src/main/java/net/onest/library/OnConfirmeListener.java
+++
@@ -1,5 +0,0 @@
-package net.onest.library;
-
-public interface OnConfirmeListener {
-    void result(String s);
-}
--- login/ShareMate/library/src/main/java/net/onest/library/OnItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package net.onest.library;
-
-public interface OnItemClickListener {
-    public void onItemClick(Object o, int position);
-}
